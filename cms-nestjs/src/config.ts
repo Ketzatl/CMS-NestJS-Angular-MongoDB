f536b806de7d252c85dda3a2204fca958b51aa18
Morgan <morgan.nott@hotmail.com>
2022-07-23 00:44:43 +0200

    Dépôt du projet

---
+++ cms-nestjs/src/config.ts
@@ -0,0 +1,6 @@
+export default {
+  mongoUri:
+    'mongodb+srv://tukho:Confiding4Doorman9Tricking1After9996math5@cluster0.u62q6.mongodb.net/?retryWrites=true&w=majority',
+  secretKey: 'Hgsftz66fs',
+  port: 8080,
+};
---
+++ cms-nestjs/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const port = 3000;
+  const app = await NestFactory.create(AppModule);
+  await app.listen(`${port}`);
+  console.log(`\n\t >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<`);
+  console.log(`\n\t >> App listening on port ${port} <<`);
+  console.log(`\n\t >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<`);
+}
+bootstrap();
