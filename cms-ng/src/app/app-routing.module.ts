bca656088949ee241a1b4a7d523d7b7b94ae4d8e
Morgan <morgan.nott@hotmail.com>
2022-07-23 16:44:02 +0200

    Ajout LazyLoading pour le composant Articles

--- cms-ng/src/app/app-routing.module.ts
+++ cms-ng/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
